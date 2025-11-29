fceae2b45ee3ef8d0cc67d443f4eff8fe9943d60
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-29 12:52:14 -0600

    h11.5 - Working(?) Shuffling
    I... finally finished the code for room shuffling, I think? It still hasn't been tested whatsoever, so that's definitely next, but it should all work in theory.
    I put this off for 10 hours lmao

--- scripts/globals/room_manager.gd
+++ scripts/globals/room_manager.gd
@@ -25,25 +25,122 @@ enum ENTRANCES {
 }
 
 ## An enum for the areas.
-enum AREAS {BRASS}
+enum AREAS {BRASS=0}
 
-## Shuffle the layout of the rooms.
-func shuffle_area(area:AREAS) -> void: 
+class area_data:
+	
+	func _by_area(a:RoomBit, b:RoomBit) -> bool:
+		var aa = a.room_size.x * a.room_size.y
+		var ba = b.room_size.x * b.room_size.y
+		return aa >= ba
+	func shuffle() -> void:
+		
+		## Shuffles all the rooms within the area.
+		
+		var rooms:Array[RoomBit]
+		
+		for key in data.keys():
+			var value := data[key]
+			
+			if value.locked: return # Ignore locked ones.
+			
+			data.erase(key) # Remove the key from data.
+			rooms.append(value) # Save the room for shuffling.
+		
+		# Sort the array by the area each room takes up.
+		rooms.sort_custom(_by_area)
+		# Place the rooms by size, erring if something goes wrong.
+		for room in rooms:
+			var pos := place(room)
+			
+			if pos == Vector2i.MIN: push_error("Failed to place ", room, " while shuffling.")
+			else: # The position is valid; relocate the room.
+				room.global_position = RoomManager.room_to_world(pos)
+			
+		
+	#	
+	#	X1 - Unassign all unlocked rooms from their slots.
+	#	X2 - Start re-placing all the rooms, from biggest to smallest area.
+	# 	|- Need a way to tell if a room can be placed regardless of its size. (Reminds of Robodungeon)
+	#	3 - Do a final pass to update the positions and door states, or do this while placing
+	#	|- Make open ones set to the initial_state field, so they can be locked OR open.
+	#	
+			
+		pass
+	
+	func _init(set_data:Dictionary[Vector2i, RoomBit], set_size:Vector2i) -> void:
+		data = set_data
+		size = set_size
+		pass
+	
+	## Try to place a room at a position, returning whether it suceeded.
+	func insert(key:Vector2i, value:RoomBit) -> bool: 
+		# IF it doesn't fit, fail.
+		if key + value.room_size > size: return false
+		
+		var x_range = range(key.x, key.x + value.room_size.x + 1)
+		var y_range = range(key.y, key.y + value.room_size.y + 1)
+		
+		# Make sure all the slots it's attempting to fill are empty.
+		for i in x_range: for j in y_range:
+			if data.has(Vector2i(i,j)): return false # Nope.
+		
+		for i in x_range: for j in y_range:
+			data[Vector2i(i,j)] = value
+		
+		return true # Placed sucessfully, so return true.
+	## Attempt to place a room anywhere in the space.
+	func place(value:RoomBit) -> Vector2i:
+		
+		var options:Array[Vector2i]
+		for i in range(size.x - value.room_size.x + 1): 
+			for j in range(size.y - value.room_size.y + 1):
+				var pos = Vector2i(i,j)
+				## Append any non-full AND possible positions.
+				if not data.has(pos): options.append(pos)
+		
+		# Try to place it in a random position, and stop if
+		# succeded, otherwise try another position. Repeat
+		# until sucess or none left.
+		while len(options) > 0:
+			var try:Vector2i = options.pick_random()
+			options.erase(try)
+			
+			if insert(try, value): return try
+		
+		return Vector2i.MIN # No options left, placing failed.
+	
+	# What rooms are filling coords or smth.
+	var data:Dictionary[Vector2i, RoomBit]
+	var size := Vector2i(10, 10)
+	
+
+var area_database:Dictionary[AREAS, area_data]
+
+## Shuffles all the rooms within an area.
+func shuffle_area(_area:AREAS) -> void: 
 	
 	## Shuffles all the rooms within an area.
-	
+#	
+#	1 - Unassign all unlocked rooms from their slots.
+#	2 - Start re-placing all the rooms, from biggest to smallest area.
+# 	|- Need a way to tell if a room can be placed regardless of its size. (Reminds of Robodungeon)
+#	3 - Do a final pass to update the positions and door states, or do this while placing
+#	|- Make open ones set to the initial_state field, so they can be locked OR open.
+#	
+#	
+#	
 	pass
 
-func shuffle() -> void:
-	for key in AREAS:
-		print(key)
+## Shuffles all rooms in all the areas.
+func shuffle() -> void: for AREA in AREAS: shuffle_area(AREA)
 
 func _ready() -> void:
 	## Make sure there are rooms to load... please...
 	assert(len(room_paths) > 0, "No rooms found in "+ ROOM_PATH)
 	
-	shuffle()
-	
+	for i in range(3):
+		print(i)
 ## Helper functions.
 
 ## Returns the given coordinates as room coordinates
