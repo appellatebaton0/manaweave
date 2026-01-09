a3543830c2dd26276794fec9d824bfc8c05fbdfd
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-01-09 13:40:14 -0600

    h23.5 - Room Loading Setup
    Started changing things in preparation to try the new way to load in rooms I decided on.

--- addons/level_editor/level_editor_pane.gd
+++ addons/level_editor/level_editor_pane.gd
@@ -3,7 +3,7 @@ extends Panel
 
 var plugin:EditorPlugin
 
-var current:RoomBit
+var current_room:RoomBit
 
 @onready var selection_display := $ScrollContainer/VBoxContainer/HBoxContainer/Selection
 @onready var entry_box := $ScrollContainer/VBoxContainer/Entries
@@ -15,38 +15,48 @@ var current:RoomBit
 
 var door_entries:Array[Control]
 
-func _ready() -> void:
-	selection.selection_changed.connect(_on_selection_change)
+func _ready() -> void: selection.selection_changed.connect(_on_selection_change)
 
 func _on_selection_change() -> void:
 	if plugin == null: return
 	
+	# Update the current room.
 	var changed := false
-	for node in selection.get_selected_nodes():
-		if node is RoomBit:
-			current = node
-			changed = true
-	if not changed: return
+	for node in selection.get_selected_nodes(): if node is RoomBit:
+		current_room = node
+		changed = true
+		break
 	
-	selection_display.text = current.name if current != null else "None"
+	# Update the panel if something's changed.
+	if changed: _update_interface()
 	
-	# Update the door entries.
+func _update_interface():
 	
+	# Remove the door entries
 	for entry in door_entries: entry.queue_free()
 	door_entries.clear()
 	
-	if current == null: return
+	# If there's no longer a current room.
+	if current_room == null:
+		# Update the selection text.
+		selection_display.text = "None"
 	
-	for door in get_current_doors(): add_door_entry(door)
+	# If there's a new current room.
+	else:
+		# Update the selection text.
+		selection_display.text = current_room.name
+		
+		# Add the new door entries.
+		for door in get_current_room_doors(): add_door_entry(door)
 
 func _on_door_adder_pressed() -> void:
-	if current == null: return
+	if current_room == null: return
 	
-	# Add the door to the current.
+	# Add the door to the current_room.
 	
 	var new = door_scene.instantiate()
 	
-	current.add_child(new)
+	current_room.add_child(new)
 	
 	new.name = "DoorBit"
 	
@@ -57,39 +67,35 @@ func _on_door_adder_pressed() -> void:
 
 ## Updates the data of the room to be correct
 func _update_room_data() -> void:
-	if current == null: return
+	if current_room == null: return
 	
-	current.doors = get_current_doors()
+	current_room.doors = get_current_room_doors()
 	
 	var new_data:Dictionary = {
 		"doors": []
 	}
 	
 	# Append the data from doors
-	for door in get_current_doors():
+	for door in get_current_room_doors():
 		new_data["doors"].append({
 			"path": door.room_path,
 			"orientation": door.orientation
 		})
 	
 	# Append the data from the room itself
-	new_data["position"] = current.global_position
+	new_data["position"] = current_room.global_position
 	
-	var undo_redo := plugin.get_undo_redo()
+	unre_action("Update Room Data", current_room, "data", new_data)
 	
-	# Use the global UndoRedo manager, EditorUndoRedoManager, to make it notice the change.
-	undo_redo.create_action("Update Room Data")
-	undo_redo.add_do_property(current, "data", new_data)
-	undo_redo.commit_action()
-	
-	print("Updated ", current, "'s data to ", current.data)
+	print("Updated ", current_room, "'s data to ", current_room.data)
 
 # Lets a node keep all its children when .filename is set to nothing.
 func localize(node):
-	node.set_owner(current)
+	node.set_owner(current_room)
 	for child in node.get_children():
 		localize(child)
 
+# Adds a door entry to the container.
 func add_door_entry(door:DoorBit):
 	var new_entry = entry_scene.instantiate()
 	new_entry.undo_redo = plugin.get_undo_redo()
@@ -101,9 +107,18 @@ func add_door_entry(door:DoorBit):
 	
 	door_entries.append(new_entry)
 
-func get_current_doors() -> Array[DoorBit]:
+func get_current_room_doors() -> Array[DoorBit]:
 	var response:Array[DoorBit]
 	
-	for child in current.get_children(): if child is DoorBit: response.append(child)
+	for child in current_room.get_children(): if child is DoorBit: response.append(child)
 	
 	return response
+
+# Perform a change via the EditorUndoRedoManager
+func unre_action(action_name:String, target:Object, property:String, new_value:Variant):
+	var undo_redo := plugin.get_undo_redo()
+	
+	# Use the global UndoRedo manager, EditorUndoRedoManager, to make it notice the change.
+	undo_redo.create_action(action_name)
+	undo_redo.add_do_property(target, property, new_value)
+	undo_redo.commit_action()
--- scripts/bits/room.gd
+++ scripts/bits/room.gd
@@ -1,6 +1,13 @@
 class_name RoomBit extends Bit
 ## The bit for a room. Manages everything pertaining to the room.
 
-@export_storage var data:Dictionary
+@export_storage var config_path := ""
 
 @export_storage var doors:Array[DoorBit]
+
+var config:ConfigFile
+func _ready() -> void:
+	
+	config = ConfigFile.new()
+	if config.load(config_path): push_warning("Failed to load the config file at path ", config_path)
+	
